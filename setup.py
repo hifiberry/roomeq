0573a0cf01bbc9331e165a364444326f8153b183
HiFiBerry <info@hifiberry.com>
2025-08-14 09:33:20 +0100

    Fix entry point for roomeq-server and add initial server implementation

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=["fastapi", "pyalsaaudio"],
     entry_points={
         'console_scripts': [
-            'roomeq-server = roomeq_server:app',
+            'roomeq-server = roomeq.roomeq_server:app',
         ],
     },
 )
